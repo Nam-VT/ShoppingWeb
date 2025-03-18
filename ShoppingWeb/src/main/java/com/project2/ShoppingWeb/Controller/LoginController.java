04de34fbf6e7cd6f56806234588ea99d09d0e376
vtn <vutiennam2004@gmail.com>
2025-03-19 00:54:51 +0700

    hoàn thiện controller

--- ShoppingWeb/src/main/java/com/project2/ShoppingWeb/Controller/LoginController.java
+++ ShoppingWeb/src/main/java/com/project2/ShoppingWeb/Controller/LoginController.java
@@ -1,5 +1,29 @@
 package com.project2.ShoppingWeb.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.project2.ShoppingWeb.Service.UserService;
+import com.project2.ShoppingWeb.DTO.LoginRequest;
+import com.project2.ShoppingWeb.Entity.User;
+
+@RestController
+@RequestMapping("/login")
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User registrationRequest) {
+        return userService.registerUser(registrationRequest);
+    }
+
+    @PostMapping("/login")
+    public LoginRequest loginUser(@RequestBody LoginRequest loginRequest) {
+        return userService.loginUser(loginRequest);
+    }
 }
