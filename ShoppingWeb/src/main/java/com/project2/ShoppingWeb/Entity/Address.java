82b5f91a9a321154dacce46c14acf7c83c1a1062
Nam-VT <160858587+Nam-VT@users.noreply.github.com>
2025-03-20 01:08:59 +0700

    Update Address.java

--- ShoppingWeb/src/main/java/com/project2/ShoppingWeb/Entity/Address.java
+++ ShoppingWeb/src/main/java/com/project2/ShoppingWeb/Entity/Address.java
@@ -2,15 +2,7 @@ package com.project2.ShoppingWeb.Entity;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
