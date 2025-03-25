b37972c03338e11fd2e59ebc5c8953b9ebd64eb5
vtn <vutiennam2004@gmail.com>
2025-03-26 01:19:22 +0700

    update call API OpenAI

---
+++ ShoppingWeb/src/main/java/com/project2/ShoppingWeb/DTO/Message.java
@@ -0,0 +1,14 @@
+package com.project2.ShoppingWeb.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String role;
+    private String content;//prompt
+}
---
+++ ShoppingWeb/src/main/java/com/project2/ShoppingWeb/Service/ChatService.java
@@ -0,0 +1,5 @@
+package com.project2.ShoppingWeb.Service;
+
+public interface ChatService {
+    String getChatResponse(String message);
+}
